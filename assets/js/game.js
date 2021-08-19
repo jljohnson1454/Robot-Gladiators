cf25e29d69d89e85768348f54c05d6ed20cd18ce
Ghidorah <jljohnson1454@gmail.com>
2021-08-19 00:36:40 -0500

    shop function

--- assets/js/game.js
+++ assets/js/game.js
@@ -76,27 +76,40 @@ var startGame = function() {
   playerAttack = 10;
   playerMoney = 10;
 
-  for(var i = 0; i < enemyNames.length; i++){
+    for(var i = 0; i < enemyNames.length; i++){
 
-    if(playerHealth > 0) {
-    window.alert("Welcome to Robot Gladiators Round " + (i + 1));
+      if(playerHealth > 0) {
+      window.alert("Welcome to Robot Gladiators Round " + (i + 1));
 
-    var pickedEnemyName = enemyNames[i];
-    enemyHealth = 50;
+      var pickedEnemyName = enemyNames[i];
+      enemyHealth = 50;
+    
+      fight(pickedEnemyName);
+
+      //if we're not at the last enemy in the array
+      if (playerHealth > 0 && i < enemyNames.length - 1) {
+        //ask if player wants to use the store before next round
+        var storeConfirm = window.confirm("The fight is over, visit the store before the next round?")
+
+        //if yes, take them to the store() function
+        if (storeConfirm) {
+
+        
+        shop();
+        }
+      }
   
-    fight(pickedEnemyName);
-  
-  }
-  else {
-    window.alert("You have lost your robot in battle! Game Over!");
-    break;
-  }
-  var endGame = function() {
-   // if player is still alive, player wins!
-    if(playerHealth >0) {
-      window.alert("Great job, you've survived the game! you now have a score of " + playerMoney + ".");
-   }
-    window.alert("You've lost your robot in battle.");
+    }
+    else {
+      window.alert("You have lost your robot in battle! Game Over!");
+      break;
+    }
+    var endGame = function() {
+    // if player is still alive, player wins!
+      if(playerHealth >0) {
+        window.alert("Great job, you've survived the game! you now have a score of " + playerMoney + ".");
+    }
+      window.alert("You've lost your robot in battle.");
 
     
 
@@ -111,9 +124,55 @@ var startGame = function() {
       window.alert("Thank you for playing Robot Gladiators! Come back soon!")
     }
   }
-}
+  }
 
   endGame();
-}
+  }
+  var shop = function() {
+    //ask player what they'd like to do
+    var shopOptionPrompt = window.prompt(
+      "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one: 'REFILL', 'UPGRADE', or 'LEAVE' to make a choice."
+    )
+    switch (shopOptionPrompt) {
+      case "REFILL": // new case
+      case "refill":
+        if (playerMoney >=7){
+        window.alert("Refilling player's health by 20 for 7 dollars.");
+
+        // increase health and decrease money
+        playerHealth = playerHealth + 20;
+        playerMoney = playerMoney - 7;}
+    else {
+      window.alert("You don't have enough money!");
+    }
+    break;
+  case "UPGRADE":
+  case "upgrade":
+    if (playerMoney >= 7){
+    window.alert("Upgrading player's attack by 6 for 7 dollars.");
+
+    // increase attack and decrease money
+    playerAttack = playerAttack + 6;
+    playerMoney = playerMoney - 7;
+    }
+    else {
+      window.alert("You don't have enough money!");
+    }
+    break;
+  case "LEAVE":
+  case "leave":
+    window.alert("Leaving the store.");
+
+    // do nothing, so function will end
+    break;
+  default:
+    window.alert("You did not pick a valid option. Try again.");
+
+    // call shop() again to force player to pick a valid option
+    shop();
+    break;
+
+    }
+  };
 
 startGame();
